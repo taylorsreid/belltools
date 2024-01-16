32b00314d6d5c52517fbb315f8ca4d4b380b8dc5
Taylor Reid <taylorsreid@gmail.com>
2024-01-16 13:17:59 -1000

    Dockerfile finally correct,

--- pb/pb_hooks/api.pb.ts
+++ pb/pb_hooks/api.pb.ts
@@ -2,12 +2,11 @@
 /// <reference path="../pb_data/types.d.ts" />
 routerAdd("GET", "/api/test", async (c) => {
     try {
-        // const data = $http.send({
-        //     url: `http://api.aviationstack.com/v1/flights?access_key=${process.env.AS_API_KEY}&arr_iata=HNL`,
-        //     method: "GET"
-        // })
-        // return c.json(data.statusCode, data.json);
-        return c.json(200, `${process.env.AS_API_KEY}`);
+        const data = $http.send({
+            url: `http://api.aviationstack.com/v1/flights?access_key=${process.env.AS_API_KEY}&arr_iata=HNL`,
+            method: "GET"
+        })
+        return c.json(data.statusCode, data.json);
         
     }
     catch (error) {
