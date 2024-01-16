f0635ec92f4a2204205967dc3ecf16676a75f13b
Taylor Reid <taylorsreid@gmail.com>
2024-01-16 13:20:56 -1000

    temporarily disable custom api endpoints

--- pb/pb_hooks/api.pb.ts
+++ pb/pb_hooks/api.pb.ts
@@ -1,15 +1,15 @@
 "use strict";
 /// <reference path="../pb_data/types.d.ts" />
-routerAdd("GET", "/api/test", async (c) => {
-    try {
-        const data = $http.send({
-            url: `http://api.aviationstack.com/v1/flights?access_key=${process.env.AS_API_KEY}&arr_iata=HNL`,
-            method: "GET"
-        })
-        return c.json(data.statusCode, data.json);
+// routerAdd("GET", "/api/test", async (c) => {
+//     try {
+//         const data = $http.send({
+//             url: `http://api.aviationstack.com/v1/flights?access_key=${process.env.AS_API_KEY}&arr_iata=HNL`,
+//             method: "GET"
+//         })
+//         return c.json(data.statusCode, data.json);
         
-    }
-    catch (error) {
-        console.log(error);
-    }
-});
+//     }
+//     catch (error) {
+//         console.log(error);
+//     }
+// });
