f4ddff6885054e8288d9e01a9347853e0729d5c1
Taylor Reid <taylorsreid@gmail.com>
2024-01-16 19:40:14 -1000

    backend mostly functional

--- pb/pb_hooks/api.pb.ts
+++ pb/pb_hooks/api.pb.ts
@@ -1,15 +1,40 @@
 "use strict";
 /// <reference path="../pb_data/types.d.ts" />
-routerAdd("GET", "/api/test", async (c) => {
-    try {
-        const data = $http.send({
+/// <reference path="./types.ts" />
+
+routerAdd("GET", "/api/flights", (c) => {
+
+    let data = {};
+
+    if (!$app.isDev()) { // does not work due to an upstream bug
+
+        // @ts-ignore
+        data = $http.send({
             url: `http://api.aviationstack.com/v1/flights?access_key=${process.env.AS_API_KEY}&arr_iata=HNL`,
             method: "GET"
         })
-        return c.json(data.statusCode, data.json);
         
+        $app.dao().saveRecord(new Record($app.dao().findCollectionByNameOrId("external_api_requests"), {
+            user: $app.dao().findAuthRecordByToken($apis.requestInfo(c).headers.authorization.substring(7), $app.settings().recordAuthToken.secret).getId()
+        }))
     }
-    catch (error) {
-        console.log(error);
+    else {
+        // @ts-ignore
+        data.statusCode = 203
+        // @ts-ignore
+        data.json = $http.send({
+            url: 'http://localhost:8090/testData.json',
+            method: 'GET'
+        }).json
     }
-});
+
+    // @ts-ignore
+    return c.json(data.statusCode, data.json)
+       
+} , $apis.requireRecordAuth());
+
+if ($app.isDev()) {
+    routerAdd("GET", "/api/mirror", (c) => {
+        return c.json(200, $apis.requestInfo(c))
+    }, $apis.requireRecordAuth());
+}
