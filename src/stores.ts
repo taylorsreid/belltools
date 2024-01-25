cf5f1a5ab5bb918c67691d15026f8d1576ac6865
Taylor Reid <taylorsreid@gmail.com>
2024-01-24 17:54:37 -1000

    bug fixed

--- src/stores.ts
+++ src/stores.ts
@@ -17,52 +17,45 @@ export const useFlightsStore = defineStore('flights', {
         
         filteredSortedFlights(): Flight[] {
 
-            // filter
-            const f = this.flights.filter((f:Flight) => {
-                if (this.hideLanded) {
-                    return !f.actual_on
-                }
-                else {
-                    return true
-                }
-                // return (this.hideLanded && !f.actual_on)
-            })
+            // hacky solution to prevent "Cannot read properties of undefined (reading 'filter'" without using async/await
+            if (Object.keys(this.apiResponse).length !== 0) {
+                // filter
+                const f = this.flights.filter((f:Flight) => {
+                    if (this.hideLanded) {
+                        return !f.actual_on
+                    }
+                    else {
+                        return true
+                    }
+                    // return (this.hideLanded && !f.actual_on)
+                })
 
-            // sort
-            switch (this.sortBy) {
-                case 'origin':
-                    return f.sort((a, b) => {
-                        a.origin.city ??= 'Unknown'
-                        b.origin.city ??= 'Unknown'
-                        return a.origin.city.localeCompare(b.origin.city) 
-                    })
-                case 'flight':
-                    return f.sort((a, b) => {
-                        a.ident_iata ??= 'Unknown'
-                        b.ident_iata ??= 'Unknown'
-                        return a.ident_iata.localeCompare(b.ident_iata)
-                    })
-                default: // ETA
-                    return f.sort((a, b) => {
-                        a.predicted_on ??= 'Unknown'
-                        b.predicted_on ??= 'Unknown'
-                        return new Date(a.predicted_on).getTime() - new Date(b.predicted_on).getTime()
-                    })
+                // sort
+                switch (this.sortBy) {
+                    case 'origin':
+                        return f.sort((a, b) => {
+                            a.origin.city ??= 'Unknown'
+                            b.origin.city ??= 'Unknown'
+                            return a.origin.city.localeCompare(b.origin.city) 
+                        })
+                    case 'flight':
+                        return f.sort((a, b) => {
+                            a.ident_iata ??= 'Unknown'
+                            b.ident_iata ??= 'Unknown'
+                            return a.ident_iata.localeCompare(b.ident_iata)
+                        })
+                    default: // ETA
+                        return f.sort((a, b) => {
+                            a.predicted_on ??= 'Unknown'
+                            b.predicted_on ??= 'Unknown'
+                            return new Date(a.predicted_on).getTime() - new Date(b.predicted_on).getTime()
+                        })
+                }
+            }
+            else {
+                return []
             }
         },
-
-        // // disabled for now due to large paginated responses
-        // filteredSortedFlights(): Flight[] {
-        //     const filtered = this.filteredFlights
-        //     switch (this.sortBy) {
-        //         case 'origin':
-        //             return filtered.sort((a, b) => { return a.departure.airport.localeCompare(b.departure.airport) })
-        //         case 'airline':
-        //             return filtered.sort((a, b) => { return a.airline.name.localeCompare(b.airline.name) })
-        //         default: // ETA
-        //             return filtered.sort((a, b) => { return new Date(a.arrival.estimated).getTime() - new Date(b.arrival.estimated).getTime() })
-        //     }
-        // }
     },
     actions: {
         async callApi(): Promise<void> {
